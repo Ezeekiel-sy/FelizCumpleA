ab1168f4a8f5e82a4e92ca81aeaff11115165c32
Ezeekiel-sy <131221971+Ezeekiel-sy@users.noreply.github.com>
2024-11-28 22:49:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ function openCardPage(image) {
     window.open("detalle.html?image=" + image, "_blank");
 }
 const messages = {
-    'fotos/carta1.jpeg': 'El día que nos conocimos, \nUn suspiro en el aire, \nMiradas que se cruzaron, \nY un amor que comenzó a nacer.',
-    'carta2.jpeg': 'En cada paso que damos, \nNos acercamos más al sol, \nY aunque la vida nos pone pruebas, \nSiempre encontraremos el amor.',
-    'carta3.jpeg': 'Tiempos de risas y de lágrimas, \nUn viaje lleno de emoción, \nCada momento juntos es eterno, \nUn latido del corazón.',
-    'carta4.jpeg': 'Aunque el tiempo pase lento, \nSiempre estaré aquí a tu lado, \nEl amor que siento por ti es inmenso, \nInquebrantable, nunca será olvidado.'
-};
+    'fotos/carta1.jpeg': 'La primera vez que te vi, sentí el suelo desmoronarse bajo mis pies, cuando me di cuenta Era más de vos que de mí mismo. Esa noche me perdí en vos. En tus ojos, tu risa, en en tus labios. En una sola noche pintaste mi mundo de colores, colores que no sabía que existían, pero que ahora me habitan. Es ridículo, pero esa noche fue suficiente. Me enamoré de cada gesto que hacía temblar el tiempo, de cada mirada que encendía los silencios, y me perdí aún más con cada beso que dibujabas en mí. Desde entonces, no he dejado de caminar por el universo que sos.',
+        'fotos/carta2.jpeg': 'Estos meses contigo han sido inimaginables. Jamás pensé que tendría a alguien como vos: fuerte, valiente, y seguir siendo bueno, dulce, tan único. Sos esa parte que siempre busqué, lo que mi alma anhelaba sin saberlo. Cada vez que estoy contigo, el mundo podría arder en llamas y no me importaría, porque con una mirada, una caricia o un beso tuyo, todo mi infierno se apaga. En ese instante, paso del caos al cielo, contigo... de la mano..',
+        'fotos/carta3.jpeg': 'Te lo prometo. No te amaré solo por lo que ves, sino por lo que somos, por lo que crecemos juntos, por cada palabra compartida, por cada beso robado a la eternidad. Mi amor por ti no morirá, será un amor que no se mide en belleza, sino en la profundidad de lo que hemos construido,en lo que hemos sido siempre, y en lo que seguiremos siendo..',
+        'fotos/carta4.jpeg': 'Importante... Sos eso indispensable, casi como respirar. Antes pensaba que podía comerme el mundo solo, pero ahora es inimaginable verme sin vos. En cada sueño, en cada meta que tengo, quiero que estés conmigo, y quiero estar a tu lado en cada cosa que logres, en cada decisión que tomes. Voy a estar contigo siempre. Podrás contar conmigo en los buenos momentos, haciendo unboxing de hamburguesas, riéndonos viendo películas súper high, disfrutando de nuestra propia burbuja. Y también en las malas, cuando no quieras saber nada del mundo, cuando sientas que todo se derrumba y quieras mandar todo a la mierda. Voy a estar ahí para consolarte, para ayudarte, para recordarte que no importa cuán oscuro parezca todo, no estás solo. Sin importar nada, voy a estar a tu lado, porque vos sos mi mundo ahora.'
+        };
 document.body.innerHTML = `
     <div class="container">
         <img src="${image}" alt="Imagen de la carta">
